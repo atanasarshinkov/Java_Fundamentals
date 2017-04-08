ef01528fb1a105a445ff65988aea58de3127be74
atanasarshinkov <atanas9705@gmail.com>
2017-04-09 01:35:12 +0300

    New Live Demo

--- src/Java_Live_Demo/LiveDemo_01.java
+++ src/Java_Live_Demo/LiveDemo_01.java
@@ -1,10 +1,15 @@
 package Java_Live_Demo;
 
-import java.util.Scanner;
+import java.util.Stack;
 
 public class LiveDemo_01 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(12);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+
 
     }
 }
